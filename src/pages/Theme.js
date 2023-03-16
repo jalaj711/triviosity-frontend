7d6a8720774e2f046c05aad9011b5722f01d5d16
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-03-16 19:22:36 +0530

    added dynamic theme loading and genres
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- src/pages/Theme.js
+++ src/pages/Theme.js
@@ -1,52 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Theme.css";
-import { useRef } from "react";
 import Flippy, { FrontSide, BackSide } from "react-flippy";
-
-import image from "../w1.jpg";
-import image1 from "../w2.jpg";
-import image2 from "../w3.jpg";
-import image3 from "../w4.jpg";
-import image5 from "../w5.jpg";
+import useContext from "../utils/Context";
+import endpoints from "../utils/APIendpoints";
+import { useNavigate } from "react-router-dom";
 
 import { useGlitch } from "react-powerglitch";
+import { ColorRing } from "react-loader-spinner";
 
-const Test = () => {
+const ThemePage = () => {
   const glitch = useGlitch();
+  const [loaded, setLoaded] = useState(false)
+  const [genres, setGenres] = useState([]);
+  const context = useContext();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setLoaded(false)
+    fetch(endpoints.LIST_GENRES, {
+      headers: {
+        Authorization: `Token ${
+          context.token || localStorage.getItem("fictionary_token")
+        }`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setLoaded(true)
+        setGenres([...res.genres, ...res.genres, ...res.genres]);
+      });
+  }, [context.token]);
+
+  const setUserGenre = (genre_id) => {
+    setLoaded(false)
+    fetch(endpoints.SET_GENRES, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${
+          context.token || localStorage.getItem("fictionary_token")
+        }`,
+      },
+      method: "POST",
+      body: JSON.stringify({ genre_id })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setLoaded(true)
+        // setGenres([...res.genres, ...res.genres, ...res.genres]);
+        navigate("/question?genre_id=" + genre_id)
+      });
+  }
   return (
     <div className="Container">
       <h2 className="theme-head" ref={glitch.ref}>
         Themes
       </h2>
 
-      <div className="main-card">
-        <div className="card-1">
-          <Flippy flipOnHover={true} flipDirection="horizontal">
-            <FrontSide>
-              <img className="image-1" src={image1}></img>
-            </FrontSide>
-            <BackSide>
-              <img className="image-1" src={image2}></img>
-            </BackSide>
-          </Flippy>
-          <p className="p-theme">Romance</p>
-        </div>
-
-        <div className="card-1">
-          <img className="image-1" src={image2}></img>
-          <p className="p-theme">Sci-Fi</p>
-        </div>
-        <div className="card-1">
-          <img className="image-1" src={image3}></img>
-          <p className="p-theme">Horror</p>
-        </div>
-        <div className="card-1">
-          <img className="image-1" src={image5}></img>
-          <p className="p-theme">Comedy</p>
-        </div>
-      </div>
+      {
+        loaded ? <div className="main-card">
+        {genres.map((elem, index) => {
+          return (
+            <div className="card-1" onClick={() => {
+              setUserGenre(elem.id)
+            }}>
+              <Flippy flipOnHover={true} flipDirection="horizontal">
+                <FrontSide>
+                  <img className="image-1" src={elem.theme_pic} alt="" />
+                </FrontSide>
+                <BackSide>
+                  <img className="image-1" src={elem.theme_pic} alt="" />
+                </BackSide>
+              </Flippy>
+              <p className="p-theme">{elem.name}</p>
+            </div>
+          );
+        })}
+      </div> : <div className="box">
+              <ColorRing
+                visible={true}
+                height="135"
+                width="135"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                colors={["#65beda", "#65beda", "#65beda", "#65beda", "#65beda"]}
+              />
+            </div>
+      }
     </div>
   );
 };
 
-export default Test;
+export default ThemePage;
